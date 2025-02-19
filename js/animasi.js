2e22b212a8056a3ee22aef2e9439e59c48dad425
DzaakhIdn <dzaakhidn@gmail.com>
2025-02-19 08:02:45 +0700

    "edit keamrin"

--- js/animasi.js
+++ js/animasi.js
@@ -1,13 +1,13 @@
 // import anime from 'animejs/lib/anime.es.js';
 
-const tombol = document.querySelector(".theme_changer");
+// const tombol = document.querySelector(".theme_changer");
 
-tombol.addEventListener("click", () => {
-    console.log("pop")
-    anime({
-        targets: ".theme_changer",
-        rotate: 360,
-        duration: 5000,
-        easing: "easeInOutSine",
-    });
-});
+// tombol.addEventListener("click", () => {
+//     console.log("pop")
+//     anime({
+//         targets: ".theme_changer",
+//         rotate: 180,
+//         duration: 1500,
+//         easing: "easeInOutSine",
+//     });
+// });
--- js/main.js
+++ js/main.js
@@ -1,34 +1,44 @@
-const dropdowns = document.querySelectorAll(".dropdown");
+document.addEventListener("DOMContentLoaded", () => {
+  const dropdowns = document.querySelectorAll(".dropdown");
 
-// dropdowns.addEventListener("click", () => {
-//     console.log("clicked");
-// })
-dropdowns.forEach((dropdown) => {
+  // dropdowns.addEventListener("click", () => {
+  //     console.log("clicked");
+  // })
+  dropdowns.forEach((dropdown) => {
     const select = dropdown.querySelector(".select");
     const caret = dropdown.querySelector(".caret");
     const menu = dropdown.querySelector(".menu");
     const options = dropdown.querySelectorAll(".menu li");
     const selected = dropdown.querySelector(".selected");
 
-
-    select.addEventListener('click', () => {
-        select.classList.toggle("select-clicked");
-        caret.classList.toggle("caret-rotate");
-        menu.classList.toggle("menu-open");
+    select.addEventListener("click", () => {
+      select.classList.toggle("select-clicked");
+      caret.classList.toggle("caret-rotate");
+      menu.classList.toggle("menu-open");
     });
 
     options.forEach((option) => {
-        option.addEventListener('click', () => {
-            selected.innerText = option.innerText;
-            select.classList.remove("select-clicked");
-            caret.classList.remove("caret-rotate");
-            menu.classList.remove("menu-open")
+      option.addEventListener("click", () => {
+        selected.innerText = option.innerText;
+        select.classList.remove("select-clicked");
+        caret.classList.remove("caret-rotate");
+        menu.classList.remove("menu-open");
 
-            options.forEach((option) => {
-                option.classList.remove("active");
-            });
-            option.classList.add("active");
-        })
+        options.forEach((option) => {
+          option.classList.remove("active");
+        });
+        option.classList.add("active");
+      });
     });
-});
+  });
 
+  const themeChanger = document.querySelector("#switch");
+  const bg = document.querySelector("#page");
+
+  themeChanger.addEventListener("click", () => {
+    // console.log("clicked");
+    // console.log(bg);
+    bg.classList.toggle("darkActive");
+    // document.body.classList.toggle("dark--body");
+  });
+});
